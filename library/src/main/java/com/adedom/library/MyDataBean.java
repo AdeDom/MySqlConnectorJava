6e50250e4a82ddd4ce98c4ed8e1a7fd0a9ab9741
AdeDom <pathiphonyw@gmail.com>
2019-11-05 17:23:42 +0700

    add text2

--- library/src/main/java/com/adedom/library/MyDataBean.java
+++ library/src/main/java/com/adedom/library/MyDataBean.java
@@ -2,20 +2,30 @@ package com.adedom.library;
 
 public class MyDataBean {
 
-    private String textView;
+    private String textView1;
+    private String textView2;
 
     public MyDataBean() {
     }
 
-    public MyDataBean(String title) {
-        this.textView = title;
+    public MyDataBean(String textView1, String textView2) {
+        this.textView1 = textView1;
+        this.textView2 = textView2;
     }
 
-    public String getTextView() {
-        return textView;
+    public String getTextView1() {
+        return textView1;
     }
 
-    public void setTextView(String textView) {
-        this.textView = textView;
+    public void setTextView1(String textView1) {
+        this.textView1 = textView1;
+    }
+
+    public String getTextView2() {
+        return textView2;
+    }
+
+    public void setTextView2(String textView2) {
+        this.textView2 = textView2;
     }
 }
